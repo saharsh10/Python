ce1863c8493d9d55175648f62d6d418cd943ee05
saharsh10 <40207756+saharsh10@users.noreply.github.com>
2020-06-16 11:45:05 -0500

    Add files via upload

---
+++ Fibonnaci(bottom up).py
@@ -0,0 +1,10 @@
+def fibonnaci(n):
+    if n == 1 or n == 2:
+        return 1
+    a = [None] * (n+1)
+    a[1] = 1
+    a[2] = 1
+    for i in range(3, n+1):
+        a[i] = a[i-1] + a[i-2]
+    return a[n]
+print(fibonnaci(6))
